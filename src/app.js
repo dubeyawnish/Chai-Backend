6a8ac1899eb254fa4cbd7e79c3cfedf72954d083
Awnish Dubey <dubeyawnish6@gmail.com>
2023-12-24 13:10:34 +0530

    Register api created

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 import express from "express"
+import cors from 'cors'
+import cookieParser from "cookie-parser"
+
+const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"})) //this is middleware provided by express to parse json data
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // this is middleware parse url 
+app.use(express.static("public"))
+app.use(cookieParser())   //from this cookie parser we save the data in cookie from backend
 
 
-const app = express()
-
+import userRouter from './routes/userRouter.js'
+app.use("/api/v1/users", userRouter);
 export { app }
